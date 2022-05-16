1ec8c99738fbe64ea2c3e80295b524d1dd38ecd6
Zakaria <zackelmaoo@hotmail.fr>
2022-05-16 03:20:49 -0400

    tweaker heroku

--- weather/settings.py
+++ weather/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoice.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'weather.urls'
